8a7fc0abc0fc1a09c00cd48baa2f644535bb088c
Matheus Alberto <maatheusalberto@hotmail.com>
2022-05-04 20:05:38 -0300

    Adicionando estrutura inicial

---
+++ my_app/src/partials/Header/Header.js
@@ -0,0 +1,35 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+
+import useStyles from './Header.style'
+
+
+const Header = () => {
+
+    const classes = useStyles()
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    My App
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
