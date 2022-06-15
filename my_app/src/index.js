7f4194ca202cb0c9aa7fc0268af0693d05b735ff
Matheus Alberto <maatheusalberto@hotmail.com>
2022-06-15 19:57:35 -0300

    initial commit

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
 import App from './App'
+import './index.css'
+
+import pink from '@material-ui/core/colors/pink';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: pink[900],
+    }
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
+  
 )
