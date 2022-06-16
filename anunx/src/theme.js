79fc8f7dfb0593bfcb80021735649ad014aa5e04
Matheus Alberto <maatheusalberto@hotmail.com>
2022-06-16 10:39:38 -0300

    Estrutura inicial + MaterialUI

---
+++ anunx/src/theme.js
@@ -0,0 +1,12 @@
+//configurações do thema material ui
+import { createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme ({
+    palette: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+export default theme
