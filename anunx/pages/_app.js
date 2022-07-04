03d2593e218bbac589ce61a2d67e51c9647dcdeb
Matheus Alberto <maatheusalberto@hotmail.com>
2022-07-04 20:10:27 -0300

    Refatorando a pagina publish - styles

--- anunx/pages/_app.js
+++ anunx/pages/_app.js
@@ -16,8 +16,8 @@ export default function MyApp(props) {
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+          <CssBaseline />
+          <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
   )
