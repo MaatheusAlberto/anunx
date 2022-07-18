4afea4c2178285ab4eebbe2bf6aa0b2650663a8a
Matheus Alberto <maatheusalberto@hotmail.com>
2022-07-18 20:57:21 -0300

    sistema de login com email e senha (nextauth)

--- anunx/pages/auth/signin/index.js
+++ anunx/pages/auth/signin/index.js
@@ -1,6 +1,7 @@
 import { Formik } from 'formik'
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import { signIn, useSession } from 'next-auth/react'
 
 import {
   Box,
@@ -18,15 +19,22 @@ import TemplateDefault from '../../../src/templates/Default'
 import { initialValues, validationSchema } from './formValues'
 import useToasty from '../../../src/contexts/Toasty'
 import useStyles from './styles'
+import Alert from '@material-ui/lab/Alert'
 
 const Signin = () => {
 
   const classes = useStyles()
   const router = useRouter()
   const { setToasty } = useToasty()
+  const { data: session, status } = useSession()
+ console.log(session, status)
 
   const handleFormSubmit = async values => {
-
+    signIn('credentials', {
+        email: values.email,
+        password: values.password,
+        callbackUrl: 'http://localhost:3000/user/dashboard'
+    })
   }
 
 return (
@@ -55,6 +63,15 @@ return (
                         }) => {
                           return (
                             <form onSubmit={handleSubmit}>
+                                {
+                                    router.query.i === '1'
+                                    ? (
+                                        <Alert severity="error" className={classes.errorMessage}>
+                                            Usuário ou senha inválidos
+                                        </Alert>
+                                    )
+                                    : null
+                                }
                                 <FormControl fullWidth error={errors.email && touched.email} className={classes.formControl}>
                                     <InputLabel> E-mail </InputLabel>
                                     <Input
