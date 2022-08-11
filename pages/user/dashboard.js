54b00a01946138dcde902c5a98dcf62571667e91
Matheus Santos <maatheusalberto@hotmail.com>
2022-08-11 11:33:57 -0300

    dashboard - listando anuncios do usuarios

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,10 +1,13 @@
 //Pagina inicial da area do usuario logado
 import { Button, Container, Grid, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import TemplateDefault from '../../src/templates/Default'
-
-import Card from '../../src/components/Card'
 import Link from 'next/link'
+import { makeStyles } from '@material-ui/core/styles'
+import { getSession } from 'next-auth/react'
+
+import dbConnect from '../../src/utils/dbConnect'
+import ProductsModel from '../../src/models/products'
+import TemplateDefault from '../../src/templates/Default'
+import Card from '../../src/components/Card'
 
 const useStyles = makeStyles((theme) => ({
   buttonAdd: {
@@ -13,9 +16,9 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const Home = () => {
+const Home = ({ products }) => {
   const classes = useStyles()
-
+  
   return (
     <TemplateDefault>
       <Container maxWidth="sm">
@@ -30,57 +33,29 @@ const Home = () => {
       </Container>
       <Container maxWidth="md">
         <Grid container spacing={4}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              image={'https://source.unsplash.com/random'}
-              title="Produto X"
-              subtitle="R$60,00"
-              actions={
-                <>
-                  <Button size="small" color="primary">
-                    Editar
-                  </Button>
-                  <Button size="small" color="primary">
-                    Remover
-                  </Button>
-                </>
-              }
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              image={'https://source.unsplash.com/random'}
-              title="Produto X"
-              subtitle="R$60,00"
-              actions={
-                <>
-                  <Button size="small" color="primary">
-                    Editar
-                  </Button>
-                  <Button size="small" color="primary">
-                    Remover
-                  </Button>
-                </>
-              }
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              image={'https://source.unsplash.com/random'}
-              title="Produto X"
-              subtitle="R$60,00"
-              actions={
-                <>
-                  <Button size="small" color="primary">
-                    Editar
-                  </Button>
-                  <Button size="small" color="primary">
-                    Remover
-                  </Button>
-                </>
-              }
-            />  
-          </Grid>
+          {
+            products.map(product => (
+              <>
+                <Grid key={product._id} item xs={12} sm={6} md={4}>
+                  <Card
+                    image={`/uploads/${product.files[0].name}`}
+                    title={products.title}
+                    subtitle={products.price}
+                    actions={
+                      <>
+                        <Button size="small" color="primary">
+                          Editar
+                        </Button>
+                        <Button size="small" color="primary">
+                          Remover
+                        </Button>
+                      </>
+                    }
+                  />
+                </Grid>
+              </>
+            ))
+          }
         </Grid>
       </Container>
     </TemplateDefault>
@@ -88,5 +63,19 @@ const Home = () => {
 }
 
 Home.requireAuth = true
+
+export async function getServerSideProps({ req }) {
+
+  const session = await getSession({ req })
+  await dbConnect()
+  
+  const products = await ProductsModel.find({'user.id': session.userId})
+  
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    }
+  }
+}
 
 export default Home
